d4b8ac9ccf77d93e065ccdf8b276b40487ee2615
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-28 08:37:56 -0700

    it will make  my custom pizza order form in index.html

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function order (size,cheese){
+  this.size = size;
+  this. sauce = 1;
+  this.cheese = cheese;
+  this.meat1= 2;
+  this.meat2= 2;
+  this.veggie1=1;
+  this.veggie2=1;
+  this.pricePizza=0;
+}
